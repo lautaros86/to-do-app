60358c1cc059e602563a1bbc2b733e47c14cff1a
Lautaro Silva <lsilva@auntap.com>
2021-09-11 20:51:12 -0300

    move apiUrl to environment

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
 
-  constructor() { }
+  url = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  sendTimeTask(task: any, time: number) {
+    return this.http.post(this.url, {
+      idTask: task.id,
+      time
+    })
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: 'https://run.mocky.io/v3/8fb29947-9932-4657-b9af-975b74d585b9',
 };
 
 /*
